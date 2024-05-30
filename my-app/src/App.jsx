c90d6af2be08d61e01b5c28ac210308ded58c7a5
Mary Power <maryy.power@gmail.com>
2024-05-30 09:07:27 -0700

    started hero files

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
 
 <Header />
+<Hero />
 <Footer />
 
     </div>
